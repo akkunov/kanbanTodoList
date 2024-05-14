fe9e9d0b7d7f8884d600cee1092f561e4d0b687a
akkunov <akunov313131@gmail.com>
2024-05-14 22:42:35 +0600

    добавил иконки так же удалеие todoTitle так же сделал его перемным а не константой

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class useLocalstorage {
 }
 
 // основной массив получает из хранилища если нет то присваевает ему пустой массив
-const todoTitle = useLocalstorage.getData('todos')|| []
+let todoTitle = useLocalstorage.getData('todos')|| []
 
 // функция которая превращает div в input чтобы редактировать
 function changeInput(e) {
@@ -91,7 +91,6 @@ function changeTodoTitleName (e, id) {
             updateTodoValue(event);
         }
     });
-    input.addEventListener('dblclick', deleteValue(id))
     input.addEventListener('blur', updateTodoValue)
     input.focus();
 }
@@ -123,8 +122,7 @@ function deleteTodo(event, id) {
 // эта функция по классу  находит родительский блок (todoBlock) потом достает
 // из датасета id и удаляет из массива todoTitle
 function deleteValue(id){
-    console.log('d', id)
-   console.log(todoTitle.filter(item => item.id !== id));
+    todoTitle = todoTitle.filter(item => item.id !== id)
     renderBlocks()
 }
 
@@ -133,8 +131,13 @@ function createTodoElement({title, id, checked}) {
     const item =
         `<div class="todo__item" data-id="${id}">
                 <div class="todo--item__actions container">
-                    <button onclick="deleteTodo(event, '${id}')" class="delete__todo">x</button>
-            </div>
+                    <button onclick="deleteTodo(event, '${id}')" class="delete__todo">
+                    <svg width="10" height="10" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.59236 0.360001L0 3.95236L1.8344 5.78675L6.36943 10.3982L1.8344 14.9332L0 16.6912L3.59236 20.36L5.42675 18.5256L10.0382 13.9141L14.5732 18.5256L16.3312 20.36L20 16.6912L18.1656 14.9332L13.5541 10.3982L18.1656 5.78675L20 3.95236L16.3312 0.360001L14.5732 2.1944L10.0382 6.72943L5.42675 2.1944L3.59236 0.360001Z" fill="black"/>
+                    </svg>
+
+                </button>
+                </div>
             <div>
                 <span 
                     class="todo__item--title" 
@@ -182,10 +185,19 @@ function renderBlocks() {
         const todoBlock = document.createElement('div');
         todoBlock.classList.add('todo--block');
         todoBlock.innerHTML = `
-            <span 
-                class="todo__title"
-                onclick="changeTodoTitleName(event, '${items.id}')"
-                ondblclick="deleteValue('${items.id}')" >${items.name}</span> 
+           <div class="todo--block-header">
+                <span 
+                    class="todo__title"
+                    onclick="changeTodoTitleName(event, '${items.id}')"
+                    ondblclick="deleteValue('${items.id}')" >${items.name}
+                </span> 
+                <button class="delete__todo" onclick="deleteValue('${items.id}')">
+                    <svg width="15" height="15" viewBox="0 0 35 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14.9936 0C12.2447 0 9.99571 2.24904 9.99571 4.99786H4.99786C2.24904 4.99786 0 7.24689 0 9.99571H34.985C34.985 7.24689 32.736 4.99786 29.9871 4.99786H24.9893C24.9893 2.24904 22.7402 0 19.9914 0H14.9936ZM4.99786 14.9936V39.0333C4.99786 39.583 5.39769 39.9829 5.94745 39.9829H29.0875C29.6373 39.9829 30.0371 39.583 30.0371 39.0333V14.9936H25.0393V32.4861C25.0393 33.8855 23.9397 34.985 22.5403 34.985C21.1409 34.985 20.0414 33.8855 20.0414 32.4861V14.9936H15.0435V32.4861C15.0435 33.8855 13.944 34.985 12.5446 34.985C11.1452 34.985 10.0457 33.8855 10.0457 32.4861V14.9936H5.04784H4.99786Z" fill="black"/>
+                    </svg>
+                </button>
+            </div>
+            
             <div class="todo__list" data-id="${items.id}">
                 ${renderTodos(items.todos)}
             </div>
